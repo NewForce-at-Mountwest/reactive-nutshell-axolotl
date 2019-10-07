6682f9d619402a0e73deb024c4392c506c8ffe5a
Jennifer <jenalexsarah@gmail.com>
2019-10-07 15:51:06 -0400

    working on event graphs

--- react-nutshell/src/components/eventReport/EventReport.js
+++ react-nutshell/src/components/eventReport/EventReport.js
@@ -7,19 +7,39 @@ import EventReportCard from "../eventReport/EventReportCard"
 class EventReportBuild extends Component {
   // set the object keys in state
   state = {
-    events: []
+    events: [],
+    eventNames: [],
+    profits: []
   };
 
 componentDidMount() {
   console.log("EVENT REPORT: ComponentDidMount");
   //call getAll from EventManager to bring back all events for a user and hang on to that data; put it in state
   EventManager.getAll().then(events => {
+// map over events and create new object of event names and gross profits
+const eventArray = events.map(singleEvent => {
+  return {
+    "event": `${singleEvent.event}`,
+    "profits": `${+singleEvent.proceeds- +singleEvent.cost}`
+  }
+})
     this.setState({
-      events: events
+      events: events,
+      eventNames: eventArray.event,
+      profits: eventArray.profits,
+      data: {
+        labels: [],
+dataset:[
+  {label: "Event Profits",
+  data:[]
+  }
+]
+      }
     })
   });
 }
-// render the statistics by event on the event report cards
+// render the statistics by event on the event report cards and a gross profit graph
+// by event name
 render () {
   return (
     <>
@@ -34,10 +54,13 @@ render () {
             />
           ))}
         </div>
+        {/* section with gross profit line graph of all events */}
+        <section>
       {/* <Line
       // options= {{responsive: true}}
       // data={}
 /> */}
+</section>
     </div>
     </>
   )
