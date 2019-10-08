3a9dbb6c6cc41df7a5742d03d9c8be90f8992dcb
Jennifer <jenalexsarah@gmail.com>
2019-10-08 10:04:55 -0400

    ready to push up for pr

--- react-nutshell/src/components/eventReport/EventReport.js
+++ react-nutshell/src/components/eventReport/EventReport.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import EventManager from "../../modules/EventManager";
-// import {line, bar, Line} from "react-chartjs-2"
 import EventReportCard from "../eventReport/EventReportCard"
 
 // build component to create report
@@ -8,8 +7,6 @@ class EventReportBuild extends Component {
   // set the object keys in state
   state = {
     events: [],
-    eventNames: [],
-    profits: []
   };
 
 componentDidMount() {
@@ -17,24 +14,13 @@ componentDidMount() {
   //call getAll from EventManager to bring back all events for a user and hang on to that data; put it in state
   EventManager.getAll().then(events => {
 // map over events and create new object of event names and gross profits
-const eventArray = events.map(singleEvent => {
+events.map(singleEvent => {
   return {
     "event": `${singleEvent.event}`,
-    "profits": `${+singleEvent.proceeds- +singleEvent.cost}`
   }
 })
     this.setState({
       events: events,
-      eventNames: eventArray.event,
-      profits: eventArray.profits,
-      data: {
-        labels: [],
-dataset:[
-  {label: "Event Profits",
-  data:[]
-  }
-]
-      }
     })
   });
 }
@@ -54,13 +40,6 @@ render () {
             />
           ))}
         </div>
-        {/* section with gross profit line graph of all events */}
-        <section>
-      {/* <Line
-      // options= {{responsive: true}}
-      // data={}
-/> */}
-</section>
     </div>
     </>
   )
