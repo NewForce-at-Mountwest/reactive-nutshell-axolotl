b65b1a046922a1269f102acfbf23a34bc46114a0
Your Name <lukemiller4779@gmail.com>
2019-10-08 11:18:13 -0400

    commit

--- react-nutshell/src/components/chat/ChatCard.js
+++ react-nutshell/src/components/chat/ChatCard.js
@@ -8,39 +8,41 @@ export default class MessageComponent extends Component {
 					type="button"
 					className="btn size1button"
 					onClick={() => {
-						this.props.history.push(`/${this.props.route}/${this.props.message.id}/edit`);
 					}}
 				>
 					Edit
 				</button>
 			);
 		} else {
-		}
-	};
-	handleFriendship = (e) => {
-		window.alert('Are you sure you want to add this friend?');
-		const friendshipObject = {
-			otherId: e.target.id,
-			userId: localStorage.getItem('userId')
-		};
+        }
 
-		return fetch(`http://localhost:1234/friends`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(friendshipObject)
-		}).then((d) => d.json()).then(()=> window.alert("They're now your friend!"))
-	};
+    };
+
+
+	// handleFriendship = (e) => {
+	// 	window.alert('Are you sure you want to add this friend?');
+	// 	const friendshipObject = {
+	// 		otherId: +e.target.id,
+	// 		userId: +localStorage.getItem('userId')
+	// 	};
+
+	// 	return fetch(`http://localhost:1234/friends`, {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'Content-Type': 'application/json'
+	// 		},
+	// 		body: JSON.stringify(friendshipObject)
+	// 	}).then((d) => d.json()).then(()=> window.alert("They're now your friend!"))
+	// };
 
 	render() {
 		return (
 			<React.Fragment>
 				<div key={this.props.message.id} className="chat">
-					<span id={this.props.message.userId} className="chat-name" onClick={this.handleFriendship}>
+					<span id={this.props.message.userId} className="chat-name" >
 						{this.props.message.user.username.split(' ')[0]}:
 					</span>{' '}
-					{this.props.message.message}
+                    <p onClick={()=>this.props.clickedMessage(this.props.message)}>{this.props.message.message}</p>
 					{this.isUser()}
 				</div>
 			</React.Fragment>
--- react-nutshell/src/components/chat/ChatList.js
+++ react-nutshell/src/components/chat/ChatList.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 import ChatCard from './ChatCard'
 import ChatManager from '../../modules/ChatManager'
 export default class ChatList extends Component {
+
 	state = {
 		userId: localStorage.getItem('userId'),
         messages: [],
-        messageInput: ""
-	};
+        messageInput: "",
+        chatToEdit: {}
+    };
+    //update chat with inline edit
+    updateChat = evt => {
+        evt.preventDefault();
+        if (evt.keyCode === 13)
+       { const editedChat = {
+          id: this.state.chatToEdit.id,
+          message: this.state.messageInput,
+          userId: this.state.chatToEdit.userId
+        };
+        ChatManager.update(editedChat)
+          .then(ChatManager.getAll)
+          .then(chats => {
+            this.setState({
+              chats: chats,
+              chatToEdit: {}
+            });
+          });}
+    };
 	scrollToBottom = () => {
 		const chatDiv = document.getElementById('chat-messages');
 		chatDiv.scrollTop = chatDiv.scrollHeight;
@@ -32,13 +52,23 @@ export default class ChatList extends Component {
               messages: messagesFromDatabase
             });
           })
-	}
+    };
+    ChangetoInput =(clickMessage)=> {
+        this.setState({
+            chatToEdit: clickMessage,
+            messageInput: clickMessage.message
+        }
+        )
+    }
 	render() {
 		return (
 			<React.Fragment>
 				<div className="chat-messages" id="chat-messages" key={this.state.userId}>
 					{this.state.messages.map((message) => {
-						return <ChatCard {...this.props} message={message} route={this.props.route} />;
+						return this.state.chatToEdit.id===message.id ?(
+                            <> <input type= "text" required className="form-control"onChange={this.handleFieldChange}id="messageInput"value={this.state.messageInput}onKeyUp={this.updateChat}/></>
+
+                        ):<ChatCard {...this.props} clickedMessage={this.ChangetoInput} message={message} route={this.props.route} />;
 					})}
 				</div>
 				<div className="chat-input">
